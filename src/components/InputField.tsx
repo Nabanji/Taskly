54dce56d975800a19d5e89c8612e8c512cc71520
Nabanji <pnabanji@gmail.com>
2025-10-23 18:07:15 +0300

    Taskly UI done

---
+++ src/components/InputField.tsx
@@ -0,0 +1,33 @@
+import type React from 'react';
+import './styles.css';
+import { useRef } from 'react';
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  return (
+    <form className="input" onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+    }}>
+        <input 
+            ref={inputRef}
+            type="text" 
+            placeholder="Add a new task"
+            className="input-box"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+        />
+        <button className="input-submit" type="submit">Go</button>
+    </form>
+  )
+}
+
+export default InputField
